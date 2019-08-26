d74784384277a755e8fabf24215b9e60a0b0a43f
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-08-26 09:31:51 -0600

    Add feature test for searching for alt fuel types

---
+++ spec/features/user_can_search_for_alt_fuel_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User can visit root page' do
+  scenario "and search for alternate fuel" do
+    visit '/'
+
+    fill_in "Search by zip...", with: "80203"
+    click_on 'Locate'
+
+    expect(current_path).to eq('/search')
+
+    expect((".stations").count).to eq 10
+  end
+end
+
+
+# Then I should see a list of the 10 closest stations within 6 miles sorted
+# by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types,
+# Distance, and Access Times
